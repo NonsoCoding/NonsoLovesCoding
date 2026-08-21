5751bfd408aa1124be5ab5c457ff1c736cd02d01
Your Name <timothyobi494@gmail.com>
2026-08-21 07:32:39 +0100

    nonso portfolio --major fix

---
+++ app/sitemap.ts
@@ -0,0 +1,13 @@
+// app/sitemap.ts
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://nonsolovescoding.tech",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+}
